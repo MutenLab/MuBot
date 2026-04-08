469f5ee2fd6c723373f6d79d787ba48399943c65
Ivan Cerrate <ivan.ccerrate@gmail.com>
2026-04-08 08:40:24 -0500

    Workaround to avoid moving when buffing

--- bash/actions/goToFoggyForestBuffSpot.sh
+++ bash/actions/goToFoggyForestBuffSpot.sh
@@ -7,10 +7,13 @@ teleport=${1:-true}
 if [ $teleport = true ]; then
 $PROJECT_DIR/bash/teleport/toFoggyForest.sh
 fi
-
 sleep 4
+
+# Workaround to avoid going back to previous auto target location
+tap_attack
+adb_tap 940 325
+sleep 0.5
+
 $PROJECT_DIR/bash/actions/switchWire.sh 1 &
 switchWirePID=$!              # Save PID
 wait $switchWirePID           # Wait to ensure it's terminated
-sleep 3
-adb_tap 585 460
