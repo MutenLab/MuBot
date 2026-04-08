e82c74e3b0d436ffd55ef29e8d025b4d3dea769d
Ivan Cerrate <ivan.ccerrate@gmail.com>
2026-04-07 21:26:53 -0500

    Improve kundunTrial script

--- bash/actions/kundunTrial.sh
+++ bash/actions/kundunTrial.sh
@@ -4,8 +4,6 @@
 source $PROJECT_DIR/config/variables.sh
 source $PROJECT_DIR/bash/utils/visionUtils.sh
 
-attackTime=20
-
 # Auto attack to make sure top buttons are hidden
 tap_auto
 sleep 1
@@ -21,6 +19,10 @@ tap_more_top_daily_goal
 # Find and tap Kundun Trial in the daily goals grid
 sleep 1
 findAndTapDailyGoalEvent $DG_KUNDUN_TRIAL
+if [ $? -ne 0 ]; then
+    echo "[$(date '+%H:%M:%S')] Kundun Trial event not found. Exiting."
+    exit 1
+fi
 
 # Enter event
 sleep 0.5
@@ -29,10 +31,13 @@ sleep 8
 
 # Move to Kundun spot
 tap_openMap
-sleep 3
+sleep 0.5
 tap_event_kundun_trial_best_location
-sleep 7
+sleep 0.5
+tap_closeMap
+sleep 6
 
-# Auto attack
-tap_auto
-sleep 30
+# Auto attack boss and wait until dead
+$PROJECT_DIR/bash/attack/smartAutoPlay.sh boss
+
+sleep 5
